a419b3d0ed067f12e17d5863a6464e4038144bf2
Shen Yong <potatguy@gmail.com>
2025-10-27 16:50:46 +0000

    write test for times.py

--- times.py
+++ times.py
@@ -22,5 +22,7 @@ def compute_overlap_time(range1, range2):
 
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    print(f'large: {large}')
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    print(compute_overlap_time(large, short))
+    print(f'short: {short}')
+    print(f'overlap time: {compute_overlap_time(large, short)}')
